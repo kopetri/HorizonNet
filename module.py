9cf685158b86ccf582bb98ea53ad9b06053f3189
sebastian <sebastian.hartwig@uni-ulm.de>
2021-11-10 18:43:22 +0100

    change to pytorch_lighting

---
+++ module.py
@@ -0,0 +1,97 @@
+import torch
+import pytorch_lightning as pl
+import torch.nn.functional as F
+from model import HorizonNet
+from inference import inference
+from eval_general import test_general
+import numpy as np
+
+
+class HorizonModel(pl.LightningModule):
+    def __init__(self, opt):
+        super().__init__()
+        self.save_hyperparameters()
+        self.opt = opt
+        self.net = HorizonNet(self.opt.backbone, not self.opt.no_rnn)
+
+        assert -1 <= self.opt.freeze_earlier_blocks and self.opt.freeze_earlier_blocks <= 4
+        if self.opt.freeze_earlier_blocks != -1:
+            b0, b1, b2, b3, b4 = self.net.feature_extractor.list_blocks()
+            blocks = [b0, b1, b2, b3, b4]
+            for i in range(self.opt.freeze_earlier_blocks + 1):
+                print('Freeze block%d' % i)
+                for m in blocks[i]:
+                    for param in m.parameters():
+                        param.requires_grad = False
+
+        if self.opt.bn_momentum:
+            for m in self.net.modules():
+                if isinstance(m, (torch.nn.BatchNorm1d, torch.nn.BatchNorm2d)):
+                    m.momentum = self.opt.bn_momentum
+
+    def forward(self, x, y_bon, y_cor):
+        losses = {}
+
+        y_bon_, y_cor_ = self.net(x)
+        losses['bon'] = F.l1_loss(y_bon_, y_bon)
+        losses['cor'] = F.binary_cross_entropy_with_logits(y_cor_, y_cor)
+        losses['total'] = losses['bon'] + losses['cor']
+
+        return losses
+
+    def on_train_epoch_start(self) -> None:
+        if self.opt.freeze_earlier_blocks != -1:
+            b0, b1, b2, b3, b4 = self.net.feature_extractor.list_blocks()
+            blocks = [b0, b1, b2, b3, b4]
+            for i in range(self.opt.freeze_earlier_blocks + 1):
+                for m in blocks[i]:
+                    m.eval()
+        
+    def training_step(self, batch, batch_idx):
+        x, y_bon, y_cor = batch
+
+        losses = self(x, y_bon, y_cor)
+
+        loss = losses['total']
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y_bon, y_cor, gt_cor_id = batch
+        x, y_bon, y_cor = x[None], y_bon[None], y_cor[None]
+
+        losses = self(x, y_bon, y_cor)
+
+        # True eval result instead of training objective
+        true_eval = dict([
+            (n_corner, {'2DIoU': [], '3DIoU': [], 'rmse': [], 'delta_1': []})
+            for n_corner in ['4', '6', '8', '10+', 'odd', 'overall']
+        ])
+        try:
+            dt_cor_id = inference(self.net, x, x.device, force_raw=True)[0]
+            dt_cor_id[:, 0] *= 1024
+            dt_cor_id[:, 1] *= 512
+        except:
+            dt_cor_id = np.array([
+                [k//2 * 1024, 256 - ((k%2)*2 - 1) * 120]
+                for k in range(8)
+            ])
+        test_general(dt_cor_id, gt_cor_id, 1024, 512, true_eval)
+        losses['2DIoU'] = torch.FloatTensor([true_eval['overall']['2DIoU']])
+        losses['3DIoU'] = torch.FloatTensor([true_eval['overall']['3DIoU']])
+        losses['rmse'] = torch.FloatTensor([true_eval['overall']['rmse']])
+        losses['delta_1'] = torch.FloatTensor([true_eval['overall']['delta_1']])
+
+    def configure_optimizers(self):
+        # Create optimizer
+        if self.opt.optim == 'SGD':
+            optimizer = torch.optim.SGD(
+                filter(lambda p: p.requires_grad, self.net.parameters()),
+                lr=self.opt.lr, momentum=self.opt.beta1, weight_decay=self.opt.weight_decay)
+        elif self.opt.optim == 'Adam':
+            optimizer = torch.optim.Adam(
+                filter(lambda p: p.requires_grad, self.net.parameters()),
+                lr=self.opt.lr, betas=(self.opt.beta1, 0.999), weight_decay=self.opt.weight_decay)
+        else:
+            raise NotImplementedError()
+
+        return optimizer
--- train.py
+++ train.py
@@ -1,254 +1,141 @@
-import os
+import sys
 import argparse
-import numpy as np
-from tqdm import trange
-from torch.utils.tensorboard import SummaryWriter
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch import optim
+import random
+import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-
-from model import HorizonNet, ENCODER_RESNET, ENCODER_DENSENET
+from model import ENCODER_RESNET, ENCODER_DENSENET
 from dataset import PanoCorBonDataset
-from misc.utils import group_weight, adjust_learning_rate, save_model, load_trained_model
-from inference import inference
-from eval_general import test_general
-
-
-def feed_forward(net, x, y_bon, y_cor):
-    x = x.to(device)
-    y_bon = y_bon.to(device)
-    y_cor = y_cor.to(device)
-    losses = {}
-
-    y_bon_, y_cor_ = net(x)
-    losses['bon'] = F.l1_loss(y_bon_, y_bon)
-    losses['cor'] = F.binary_cross_entropy_with_logits(y_cor_, y_cor)
-    losses['total'] = losses['bon'] + losses['cor']
-
-    return losses
+from module import HorizonModel
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--id', required=True,
-                        help='experiment id to name checkpoints and logs')
-    parser.add_argument('--ckpt', default='./ckpt',
-                        help='folder to output checkpoints')
-    parser.add_argument('--logs', default='./logs',
-                        help='folder to logging')
-    parser.add_argument('--pth', default=None,
-                        help='path to load saved checkpoint.'
-                             '(finetuning)')
+
+    parser.add_argument('--seed', default=None, type=int, help='Random Seed')
+    parser.add_argument('--precision', default=16,   type=int, help='16 to use Mixed precision (AMP O2), 32 for standard 32 bit float training')
+    parser.add_argument('--gpus', type=int, default=-1, help='Number of GPUs')
+    parser.add_argument('--dev', action='store_true', help='Activate Lightning Fast Dev Run for debugging')
+    parser.add_argument('--overfit', action='store_true', help='If this flag is set the network is overfit to 1 batch')
+    parser.add_argument('--min_epochs', default=10, type=int, help='Minimum number of epochs.')
+    parser.add_argument('--max_epochs', default=300, type=int, help='Maximum number ob epochs to train')
+    parser.add_argument('--worker', default=6, type=int, help='Number of workers for data loader')
+    parser.add_argument('--find_learning_rate', action='store_true', help="Finding learning rate.")
+    parser.add_argument('--detect_anomaly', action='store_true', help='Enables pytorch anomaly detection')
+
+    parser.add_argument('--pth', default=None, help='path to load saved checkpoint. (finetuning)')
     # Model related
-    parser.add_argument('--backbone', default='resnet50',
-                        choices=ENCODER_RESNET + ENCODER_DENSENET,
-                        help='backbone of the network')
-    parser.add_argument('--no_rnn', action='store_true',
-                        help='whether to remove rnn or not')
+    parser.add_argument('--backbone', default='resnet50', choices=ENCODER_RESNET + ENCODER_DENSENET, help='backbone of the network')
+    parser.add_argument('--no_rnn', action='store_true', help='whether to remove rnn or not')
     # Dataset related arguments
-    parser.add_argument('--train_root_dir', default='data/layoutnet_dataset/train',
-                        help='root directory to training dataset. '
-                             'should contains img, label_cor subdirectories')
-    parser.add_argument('--valid_root_dir', default='data/layoutnet_dataset/valid',
-                        help='root directory to validation dataset. '
-                             'should contains img, label_cor subdirectories')
-    parser.add_argument('--no_flip', action='store_true',
-                        help='disable left-right flip augmentation')
-    parser.add_argument('--no_rotate', action='store_true',
-                        help='disable horizontal rotate augmentation')
-    parser.add_argument('--no_gamma', action='store_true',
-                        help='disable gamma augmentation')
-    parser.add_argument('--no_pano_stretch', action='store_true',
-                        help='disable pano stretch')
-    parser.add_argument('--num_workers', default=6, type=int,
-                        help='numbers of workers for dataloaders')
+    parser.add_argument('--train_root_dir', default='data/layoutnet_dataset/train', help='root directory to training dataset. should contains img, label_cor subdirectories')
+    parser.add_argument('--valid_root_dir', default='data/layoutnet_dataset/valid', help='root directory to validation dataset. should contains img, label_cor subdirectories')
+    parser.add_argument('--no_flip', action='store_true', help='disable left-right flip augmentation')
+    parser.add_argument('--no_rotate', action='store_true', help='disable horizontal rotate augmentation')
+    parser.add_argument('--no_gamma', action='store_true', help='disable gamma augmentation')
+    parser.add_argument('--no_pano_stretch', action='store_true', help='disable pano stretch')
     # optimization related arguments
     parser.add_argument('--freeze_earlier_blocks', default=-1, type=int)
-    parser.add_argument('--batch_size_train', default=4, type=int,
-                        help='training mini-batch size')
-    parser.add_argument('--batch_size_valid', default=2, type=int,
-                        help='validation mini-batch size')
-    parser.add_argument('--epochs', default=300, type=int,
-                        help='epochs to train')
-    parser.add_argument('--optim', default='Adam',
-                        help='optimizer to use. only support SGD and Adam')
-    parser.add_argument('--lr', default=1e-4, type=float,
-                        help='learning rate')
-    parser.add_argument('--lr_pow', default=0.9, type=float,
-                        help='power in poly to drop LR')
-    parser.add_argument('--warmup_lr', default=1e-6, type=float,
-                        help='starting learning rate for warm up')
-    parser.add_argument('--warmup_epochs', default=0, type=int,
-                        help='numbers of warmup epochs')
-    parser.add_argument('--beta1', default=0.9, type=float,
-                        help='momentum for sgd, beta1 for adam')
-    parser.add_argument('--weight_decay', default=0, type=float,
-                        help='factor for L2 regularization')
+    parser.add_argument('--batch_size_train', default=4, type=int, help='training mini-batch size')
+    parser.add_argument('--batch_size_valid', default=2, type=int, help='validation mini-batch size')
+    parser.add_argument('--optim', default='Adam', help='optimizer to use. only support SGD and Adam')
+    parser.add_argument('--lr', default=1e-4, type=float, help='learning rate')
+    parser.add_argument('--lr_pow', default=0.9, type=float, help='power in poly to drop LR')
+    parser.add_argument('--warmup_lr', default=1e-6, type=float, help='starting learning rate for warm up')
+    parser.add_argument('--warmup_epochs', default=0, type=int, help='numbers of warmup epochs')
+    parser.add_argument('--beta1', default=0.9, type=float, help='momentum for sgd, beta1 for adam')
+    parser.add_argument('--weight_decay', default=0, type=float, help='factor for L2 regularization')
     parser.add_argument('--bn_momentum', type=float)
-    # Misc arguments
-    parser.add_argument('--no_cuda', action='store_true',
-                        help='disable cuda')
-    parser.add_argument('--seed', default=594277, type=int,
-                        help='manual seed')
-    parser.add_argument('--disp_iter', type=int, default=1,
-                        help='iterations frequency to display')
-    parser.add_argument('--save_every', type=int, default=25,
-                        help='epochs frequency to save state_dict')
+
     args = parser.parse_args()
-    device = torch.device('cpu' if args.no_cuda else 'cuda')
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    os.makedirs(os.path.join(args.ckpt, args.id), exist_ok=True)
 
-    # Create dataloader
-    dataset_train = PanoCorBonDataset(
-        root_dir=args.train_root_dir,
-        flip=not args.no_flip, rotate=not args.no_rotate, gamma=not args.no_gamma,
-        stretch=not args.no_pano_stretch)
-    loader_train = DataLoader(dataset_train, args.batch_size_train,
-                              shuffle=True, drop_last=True,
-                              num_workers=args.num_workers,
-                              pin_memory=not args.no_cuda,
-                              worker_init_fn=lambda x: np.random.seed())
-    if args.valid_root_dir:
-        dataset_valid = PanoCorBonDataset(
-            root_dir=args.valid_root_dir, return_cor=True,
-            flip=False, rotate=False, gamma=False,
-            stretch=False)
+    if args.detect_anomaly:
+        print("Enabling anomaly detection")
+        torch.autograd.set_detect_anomaly(True)
+    
+    # windows safe
+    if sys.platform in ["win32"]:
+        args.worker = 0
 
-    # Create model
-    if args.pth is not None:
-        print('Finetune model is given.')
-        print('Ignore --backbone and --no_rnn')
-        net = load_trained_model(HorizonNet, args.pth).to(device)
-    else:
-        net = HorizonNet(args.backbone, not args.no_rnn).to(device)
+    # Manage Random Seed
+    if args.seed is None: # Generate random seed if none is given
+        args.seed = random.randrange(4294967295) # Make sure it's logged
+    pl.seed_everything(args.seed)
 
-    assert -1 <= args.freeze_earlier_blocks and args.freeze_earlier_blocks <= 4
-    if args.freeze_earlier_blocks != -1:
-        b0, b1, b2, b3, b4 = net.feature_extractor.list_blocks()
-        blocks = [b0, b1, b2, b3, b4]
-        for i in range(args.freeze_earlier_blocks + 1):
-            print('Freeze block%d' % i)
-            for m in blocks[i]:
-                for param in m.parameters():
-                    param.requires_grad = False
+    callbacks = []
 
-    if args.bn_momentum:
-        for m in net.modules():
-            if isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
-                m.momentum = args.bn_momentum
+    callbacks += [pl.callbacks.lr_monitor.LearningRateMonitor()]
 
-    # Create optimizer
-    if args.optim == 'SGD':
-        optimizer = optim.SGD(
-            filter(lambda p: p.requires_grad, net.parameters()),
-            lr=args.lr, momentum=args.beta1, weight_decay=args.weight_decay)
-    elif args.optim == 'Adam':
-        optimizer = optim.Adam(
-            filter(lambda p: p.requires_grad, net.parameters()),
-            lr=args.lr, betas=(args.beta1, 0.999), weight_decay=args.weight_decay)
-    else:
-        raise NotImplementedError()
+    callbacks += [pl.callbacks.ModelCheckpoint(
+        verbose=True,
+        save_top_k=1,
+        filename='{epoch}-{valid_acc}',
+        monitor='valid_acc',
+        mode='max'
+    )]
 
-    # Create tensorboard for monitoring training
-    tb_path = os.path.join(args.logs, args.id)
-    os.makedirs(tb_path, exist_ok=True)
-    tb_writer = SummaryWriter(log_dir=tb_path)
+    use_gpu = not args.gpus == 0
+
+    trainer = pl.Trainer(
+        log_gpu_memory=False,
+        fast_dev_run=args.dev,
+        profiler=False,
+        gpus=args.gpus,
+        overfit_batches=1 if args.overfit else 0,
+        precision=args.precision if use_gpu else 32,
+        amp_level='O2' if use_gpu else None,
+        min_epochs=args.min_epochs,
+        max_epochs=args.max_epochs,
+        logger=pl.loggers.TensorBoardLogger("result", name="HorizonNet"),
+        callbacks=callbacks
+    )
+
+    yaml = args.__dict__
+    yaml.update({
+            'random_seed': args.seed,
+            'gpu_name': torch.cuda.get_device_name(0) if use_gpu else None,
+            'gpu_capability': torch.cuda.get_device_capability(0) if use_gpu else None
+            })
+
+    train_dataset = PanoCorBonDataset(root_dir=args.train_root_dir, flip=not args.no_flip, rotate=not args.no_rotate, gamma=not args.no_gamma, stretch=not args.no_pano_stretch)
+    val_dataset   = PanoCorBonDataset(root_dir=args.valid_root_dir, return_cor=True, flip=False, rotate=False, gamma=False, stretch=False)
+    
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.worker
+    )
+
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=args.worker
+    )
 
     # Init variable
-    args.warmup_iters = args.warmup_epochs * len(loader_train)
-    args.max_iters = args.epochs * len(loader_train)
+    args.warmup_iters = args.warmup_epochs * len(train_loader)
+    args.max_iters = args.epochs * len(train_loader)
     args.running_lr = args.warmup_lr if args.warmup_epochs > 0 else args.lr
     args.cur_iter = 0
     args.best_valid_score = 0
 
-    # Start training
-    for ith_epoch in trange(1, args.epochs + 1, desc='Epoch', unit='ep'):
+    
+    model = HorizonModel(args)
 
-        # Train phase
-        net.train()
-        if args.freeze_earlier_blocks != -1:
-            b0, b1, b2, b3, b4 = net.feature_extractor.list_blocks()
-            blocks = [b0, b1, b2, b3, b4]
-            for i in range(args.freeze_earlier_blocks + 1):
-                for m in blocks[i]:
-                    m.eval()
-        iterator_train = iter(loader_train)
-        for _ in trange(len(loader_train),
-                        desc='Train ep%s' % ith_epoch, position=1):
-            # Set learning rate
-            adjust_learning_rate(optimizer, args)
 
-            args.cur_iter += 1
-            x, y_bon, y_cor = next(iterator_train)
+    if args.find_learning_rate:
+        # Run learning rate finder
+        lr_finder = trainer.tuner.lr_find(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+        suggested_lr = lr_finder.suggestion()
+        print("Old learning rate: ", args.learning_rate)
+        args.learning_rate = suggested_lr
+        print("Suggested learning rate: ", args.learning_rate)
+    else:
+        trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
-            losses = feed_forward(net, x, y_bon, y_cor)
-            for k, v in losses.items():
-                k = 'train/%s' % k
-                tb_writer.add_scalar(k, v.item(), args.cur_iter)
-            tb_writer.add_scalar('train/lr', args.running_lr, args.cur_iter)
-            loss = losses['total']
 
-            # backprop
-            optimizer.zero_grad()
-            loss.backward()
-            nn.utils.clip_grad_norm_(net.parameters(), 3.0, norm_type='inf')
-            optimizer.step()
-
-        # Valid phase
-        net.eval()
-        if args.valid_root_dir:
-            valid_loss = {}
-            for jth in trange(len(dataset_valid),
-                            desc='Valid ep%d' % ith_epoch, position=2):
-                x, y_bon, y_cor, gt_cor_id = dataset_valid[jth]
-                x, y_bon, y_cor = x[None], y_bon[None], y_cor[None]
-                with torch.no_grad():
-                    losses = feed_forward(net, x, y_bon, y_cor)
-
-                    # True eval result instead of training objective
-                    true_eval = dict([
-                        (n_corner, {'2DIoU': [], '3DIoU': [], 'rmse': [], 'delta_1': []})
-                        for n_corner in ['4', '6', '8', '10+', 'odd', 'overall']
-                    ])
-                    try:
-                        dt_cor_id = inference(net, x, device, force_raw=True)[0]
-                        dt_cor_id[:, 0] *= 1024
-                        dt_cor_id[:, 1] *= 512
-                    except:
-                        dt_cor_id = np.array([
-                            [k//2 * 1024, 256 - ((k%2)*2 - 1) * 120]
-                            for k in range(8)
-                        ])
-                    test_general(dt_cor_id, gt_cor_id, 1024, 512, true_eval)
-                    losses['2DIoU'] = torch.FloatTensor([true_eval['overall']['2DIoU']])
-                    losses['3DIoU'] = torch.FloatTensor([true_eval['overall']['3DIoU']])
-                    losses['rmse'] = torch.FloatTensor([true_eval['overall']['rmse']])
-                    losses['delta_1'] = torch.FloatTensor([true_eval['overall']['delta_1']])
-
-                for k, v in losses.items():
-                    valid_loss[k] = valid_loss.get(k, 0) + v.item() * x.size(0)
-
-            for k, v in valid_loss.items():
-                k = 'valid/%s' % k
-                tb_writer.add_scalar(k, v / len(dataset_valid), ith_epoch)
-
-            # Save best validation loss model
-            now_valid_score = valid_loss['3DIoU'] / len(dataset_valid)
-            print('Ep%3d %.4f vs. Best %.4f' % (ith_epoch, now_valid_score, args.best_valid_score))
-            if now_valid_score > args.best_valid_score:
-                args.best_valid_score = now_valid_score
-                save_model(net,
-                           os.path.join(args.ckpt, args.id, 'best_valid.pth'),
-                           args)
-
-        # Periodically save model
-        if ith_epoch % args.save_every == 0:
-            save_model(net,
-                       os.path.join(args.ckpt, args.id, 'epoch_%d.pth' % ith_epoch),
-                       args)
+   
